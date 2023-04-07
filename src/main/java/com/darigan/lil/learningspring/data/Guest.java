5f57b72c44285971949b0397fe9dcad0b2691ef1
Darigan, David <david.darigan@dell.com>
2023-04-07 21:10:38 +0100

    Replace guest.toString with lombok

--- src/main/java/com/darigan/lil/learningspring/data/Guest.java
+++ src/main/java/com/darigan/lil/learningspring/data/Guest.java
@@ -2,42 +2,22 @@ package com.darigan.lil.learningspring.data;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@ToString
 public class Guest {
-    @Id
-    private long guestId;
-
+    @Id private long guestId;
     private String firstName;
-
     private String lastName;
-
     private String emailAddress;
-
     private String address;
-
     private String country;
-
     private String state;
-
     private String phoneNumber;
 
-
-    @Override
-    public String toString() {
-        return "Guest{" +
-                "id=" + guestId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", emailAddress='" + emailAddress + '\'' +
-                ", address='" + address + '\'' +
-                ", country='" + country + '\'' +
-                ", state='" + state + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
 }
