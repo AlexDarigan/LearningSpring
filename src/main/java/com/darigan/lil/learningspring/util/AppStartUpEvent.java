d26045a81f3e0a57f2e8701d7861269ea7c2dfa0
Darigan, David <david.darigan@dell.com>
2023-04-07 13:41:59 +0100

    Add Guest, GuestRepo & Test in AppStartUpEvent

--- src/main/java/com/darigan/lil/learningspring/util/AppStartUpEvent.java
+++ src/main/java/com/darigan/lil/learningspring/util/AppStartUpEvent.java
@@ -1,5 +1,7 @@
 package com.darigan.lil.learningspring.util;
 
+import com.darigan.lil.learningspring.data.Guest;
+import com.darigan.lil.learningspring.data.GuestRepository;
 import com.darigan.lil.learningspring.data.Room;
 import com.darigan.lil.learningspring.data.RoomRepository;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -10,13 +12,17 @@ import org.springframework.stereotype.Component;
 public class AppStartUpEvent implements ApplicationListener<ApplicationStartedEvent> {
 
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
 
-    public AppStartUpEvent(RoomRepository roomRepository) {
+    public AppStartUpEvent(RoomRepository roomRepository, GuestRepository guestRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
     }
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
         Iterable<Room> rooms = roomRepository.findAll();
         rooms.forEach(System.out::println);
+        Iterable<Guest> guests = guestRepository.findAll();
+        guests.forEach(System.out::println);
     }
 }
