b20f0f8b790e68aecba7ee97dbf0164e31c6d453
Darigan, David <david.darigan@dell.com>
2023-04-07 09:39:02 +0100

    Create Spring Web Project

---
+++ src/test/java/com/darigan/lil/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darigan.lil.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
